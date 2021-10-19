1baa6e9759c4f1cbeb8ffa3c326a3972a4e290e4
adela1806 <emyhousebnb@gmail.com>
2021-10-20 00:25:42 +0700

    add show hide function to menu

--- web/src/component/Header/index.jsx
+++ web/src/component/Header/index.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import { React, useState } from "react";
 import { AiFillDribbbleCircle } from "react-icons/ai";
+import { FaBars } from "react-icons/fa";
 
 import "../../assets/css/header.scss";
 
 const Header = () => {
-  const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId);
-
-    if (toggle && nav) {
-      toggle.addEventListener("click", () => {
-        nav.classList.toggle("show");
-      });
-    }
+  const [showMenu, setShowMenu] = useState(true);
+  const handlerClickMenu = () => {
+    setShowMenu(!showMenu);
   };
 
-  showMenu("menu-toggle", "nav-menu");
+  //   const showMenu = (toggleId, navId) => {
+  //     const toggle = document.getElementById(toggleId),
+  //       nav = document.getElementById(navId);
+
+  //     if (toggle && nav) {
+  //       toggle.addEventListener("click", () => {
+  //         nav.classList.toggle("show");
+  //       });
+  //     }
+  //   };
+  //showMenu("menu-toggle", "nav-menu");
+
   return (
     <header className="header">
       <nav className="nav bd-grid">
@@ -23,7 +29,7 @@ const Header = () => {
           <AiFillDribbbleCircle />
         </div>
 
-        <div className="nav__menu" id="nav-menu">
+        <div className={"nav__menu " + (showMenu ? "show" : "")} id="nav-menu">
           <ul className="nav__list">
             <li className="nav__item">
               <a className="nav__link" href="">
@@ -57,7 +63,13 @@ const Header = () => {
           </ul>
         </div>
 
-        <div className="nav__menu--toggle" id="menu-toggle"></div>
+        <div
+          className="nav__menu--toggle"
+          id="menu-toggle"
+          onClick={handlerClickMenu}
+        >
+          <FaBars />
+        </div>
       </nav>
     </header>
   );
